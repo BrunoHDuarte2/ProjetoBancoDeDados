d988a307f1c8014e39c4b1c8043dcc02bc9607e5
brunotarquinio <107733709+brunotarquinio@users.noreply.github.com>
2025-01-21 01:48:40 -0300

    Create Criacao_das_tabelas.sql

---
+++ Scripts SQL/Criacao_das_tabelas.sql
@@ -0,0 +1,53 @@
+CREATE TABLE Usuario (
+	Cpf varchar(11) PRIMARY KEY,
+	Nome varchar(35) NOT NULL
+);
+
+CREATE TABLE Pagamento (
+	id_pagamento int PRIMARY KEY,
+	Valor int NOT NULL,
+	Data varchar(10) NOT NULL,
+	Status varchar(20) NOT NULL
+);
+
+CREATE TABLE Item (
+	id_item int PRIMARY KEY,
+	Nome varchar(30) NOT NULL,
+	Descrição varchar(100) NOT NULL,
+	Preco int NOT NULL,
+	Data_lancamento varchar(10) NOT NULL,
+  FOREIGN KEY (id_produtora) REFERENCES Produtora (Id_produtora)
+);
+
+CREATE TABLE Produtora (
+	Id_produtora varchar(14) PRIMARY KEY,
+	Nome varchar(40) NOT NULL
+);
+
+CREATE TABLE Avaliacao (
+	id_avaliacao int PRIMARY KEY,
+	Nota int NOT NULL,
+	Comentario varchar(150) NOT NULL
+);
+
+CREATE TABLE Wishlist (
+	Id int PRIMARY KEY,
+	FOREIGN KEY (id_usuario) REFERENCES Usuario (Cpf)
+);
+
+CREATE TABLE Inventario (
+	Id_inventario int PRIMARY KEY,
+	FOREIGN KEY (id_usuario) REFERENCES Usuario (Cpf),
+	FOREIGN KEY (id_item) REFERENCES Item (id_item)
+	
+);
+
+CREATE TABLE Carteira (
+	FOREIGN KEY (id_usuario) REFERENCES Usuario (Cpf),
+	Saldo int NOT NULL
+);
+
+CREATE TABLE Carrinho_compra (
+	FOREIGN KEY (id_item) REFERENCES Item (id_item),
+	FOREIGN KEY (id_usuario) REFERENCES Usuario (Cpf)
+);
